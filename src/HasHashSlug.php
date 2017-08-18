8f2acea271bbd57b0ae362074c372bd490bbbd37
Balázs Kovács <balping314@gmail.com>
2017-08-18 21:02:13 +0200

    Remove hacky where function
    Because it has been possible to do this in a clear way, since this PR was merged: https://github.com/laravel/framework/pull/20618

--- src/HasHashSlug.php
+++ src/HasHashSlug.php
@@ -98,7 +98,8 @@ trait HasHashSlug {
 	}
 
 	/**
-	 * Used in explicit model binding if no callback
+	 * Used in implicit model binding AND
+	 * used in explicit model binding if no callback
 	 * is specified, eg: Route::model('post', Post::class)
 	 * 
 	 * @param  string $slug
@@ -107,26 +108,6 @@ trait HasHashSlug {
 	public function resolveRouteBinding($slug){
 		$id = static::decodeSlug($slug);
 		return parent::where($this->getKeyName(), $id)->first();
-	}
-
-	/**
-	 * Terrible hack to make automatic implicit route model binding possible
-	 *
-	 * @see \Illuminate\Routing\ImplicitRouteBinding::resolveForRoute
-	 * 
-	 * @param  string|array|\Closure  $column
-	 * @param  string  $operator
-	 * @param  mixed   $value
-	 * @param  string  $boolean
-	 * @return \Illuminate\Database\Query\Builder
-	 */
-	public function where(... $arguments){
-		if($arguments[0] == 'hashslug'){
-			$id = static::decodeSlug($arguments[1]);
-			return parent::where($this->getKeyName(), $id);
-		}else{
-			return parent::where(... $arguments);
-		}
 	}
 
 	/**
